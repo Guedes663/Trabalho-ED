70ddec1f1cbf72856f459be5583a266a947b7cc1
Guedes663 <125224180+Guedes663@users.noreply.github.com>
2023-03-03 11:19:56 -0300

    segundo commit

--- arquivoJgrasp/JogoDaVida.java
+++ arquivoJgrasp/JogoDaVida.java
@@ -7,9 +7,9 @@ public class JogoDaVida {
   private int matriz[][];
   private int matrizNeighbors[][];
   private int alive;
-  private int dead;
   private int line;
   private int column;
+  private String response;
 
   public void getSize() {
 
@@ -35,6 +35,78 @@ public class JogoDaVida {
 
   }
 
+  public void selectGame() {
+
+    // Método que seleciona o jogo.
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(
+        "Você deseja jogar o jogo da vida ou o jogo da aposta ?\n\nDigite 1 para jogo da vida ou 2 para jogo da aposta");
+    this.response = scanner.next().toLowerCase();
+
+    if (response.equals("1")) {
+      jogoDaVida();
+
+    } else {
+      jogoDaAposta();
+    }
+
+  }
+
+  public void jogoDaVida() {
+
+    // Método que executa o jogo da vida.
+
+    Scanner scanner = new Scanner(System.in);
+    while (response.equals("1")) {
+      System.out.println("\n");
+
+      printMatriz();
+      nextGeneration();
+
+      System.out.println("Você deseja continuar ? (Digite 1 para continuar ou 2 para sair)");
+      this.response = scanner.next().toLowerCase();
+    }
+  }
+
+  public void jogoDaAposta() {
+
+    // Método que executa o jogo da aposta.
+
+    Scanner scanner = new Scanner(System.in);
+    int counter = 0;
+    this.response = "1";
+
+    while (response.equals("1")) {
+      System.out.println("Digite a quantidade de células que estarão vivas:");
+      int bet = scanner.nextInt();
+
+      nextGeneration();
+      printMatriz();
+
+      for (int i = 0; i < this.matriz.length; i++) {
+        for (int j = 0; j < this.matriz[i].length; j++) {
+          if (matriz[i][j] == 1) {
+            counter = counter + 1;
+          }
+        }
+      }
+
+      if (counter == bet) {
+        System.out.println("Parabéns, você acertou todos os " + bet + " 1!");
+      } else {
+        int acertos = Math.min(counter, bet);
+        int erros = Math.max(counter, bet) - acertos;
+        System.out.println("Você acertou " + acertos + " errou " + erros);
+      }
+
+      counter = 0;
+
+      System.out.println("Você deseja continuar ? (Digite 1 para continuar ou 2 para sair)");
+      this.response = scanner.next().toLowerCase();
+    }
+  }
+  
   public void printMatriz() {
 
     // Método que imprime a matriz no console.
@@ -46,6 +118,42 @@ public class JogoDaVida {
       System.out.println();
     }
     System.out.println();
+  }
+
+  public void nextGeneration() {
+
+    // Método que substitui a matriz para a nova geração.
+
+    int[][] newGeneration = new int[matriz.length][matriz[0].length];
+
+    for (int i = 0; i < this.matriz.length; i++) {
+      for (int j = 0; j < this.matriz[i].length; j++) {
+
+        this.line = i;
+        this.column = j;
+
+        getNeighbors();
+        checkNeighbors();
+
+        if (this.matriz[i][j] == 1) {
+          if (this.alive < 2 || this.alive > 3) {
+            newGeneration[i][j] = 0;
+          } else {
+            newGeneration[i][j] = 1;
+          }
+        } else {
+          if (this.alive == 3) {
+            newGeneration[i][j] = 1;
+          } else {
+            newGeneration[i][j] = 0;
+          }
+
+        }
+
+        this.alive = 0;
+      }
+    }
+    this.matriz = newGeneration;
   }
 
   public void getNeighbors() {
@@ -86,81 +194,19 @@ public class JogoDaVida {
 
     if (up == 1) {
       this.alive = this.alive + 1;
-    } else {
-      this.dead = this.dead + 1;
     }
 
     if (right == 1) {
       this.alive = this.alive + 1;
-    } else {
-      this.dead = this.dead + 1;
     }
 
     if (down == 1) {
       this.alive = this.alive + 1;
-    } else {
-      this.dead = this.dead + 1;
     }
 
     if (left == 1) {
       this.alive = this.alive + 1;
-    } else {
-      this.dead = this.dead + 1;
     }
   }
 
-  public void nextGeneration() {
-
-    // Método que substitui a matriz para a nova geração.
-
-    int[][] newGeneration = new int[matriz.length][matriz[0].length];
-
-    for (int i = 0; i < this.matriz.length; i++) {
-      for (int j = 0; j < this.matriz[i].length; j++) {
-
-        this.line = i;
-        this.column = j;
-
-        getNeighbors();
-        checkNeighbors();
-
-        if (this.matriz[i][j] == 1) {
-          if (this.alive < 2 || this.alive > 3) {
-            newGeneration[i][j] = 0;
-          } else {
-            newGeneration[i][j] = 1;
-          }
-        } else {
-          if (this.alive == 3) {
-            newGeneration[i][j] = 1;
-          } else {
-            newGeneration[i][j] = 0;
-          }
-
-        }
-
-        this.alive = 0;
-        this.dead = 0;
-      }
-    }
-    this.matriz = newGeneration;
-  }
-
-  public void mustContinue() {
-
-    // Método que pergunta e imprimi a próxima geração se o usuário desejar.
-
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Você deseja imprimir a nova geração ?");
-    String response = scanner.next().toLowerCase();
-
-    while (response.equals("sim") || response.equals("s") || response.equals("ss")) {
-      nextGeneration();
-      printMatriz();
-
-      System.out.println("Voce deseja continuar ?");
-      response = scanner.next().toLowerCase();
-    }
-    System.out.println("Goodbye!");
-  }
 }
--- arquivoJgrasp/Main.java
+++ arquivoJgrasp/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     gameManager.getSize();
     gameManager.fillMatriz();
-    gameManager.printMatriz();
-    gameManager.mustContinue();
+    gameManager.selectGame();
   }
 }
